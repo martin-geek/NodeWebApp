da2653ce91308fd632078430ce171aa2b4f200c1
Martin Schmidt <martin-geek@live.com>
2016-12-04 20:16:59 -0600

    Added Express

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("<html><body><h1>Express</h1></body></html>");
+});
+
+app.get("/api/users", function(req,res){
+    res.set("Content-Type", "application/json");
+    res.send({name: "Schmidt", isValid: true, group: "Admin"})
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
